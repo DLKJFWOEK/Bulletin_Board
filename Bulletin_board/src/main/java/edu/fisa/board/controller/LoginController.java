8cc71e420f9cebad9be26a1f429a597d11b12431
resilience <frog4821@naver.com>
2024-02-16 20:07:17 +0900

    Security 기능 제외 게시판 구현 완료

---
+++ Bulletin_board/src/main/java/edu/fisa/board/controller/LoginController.java
@@ -0,0 +1,37 @@
+package edu.fisa.board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.fisa.board.service.AuthenticationService;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	private AuthenticationService authenticationService;
+	
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login"; 
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password, Model model){
+    	System.out.println("0000 ---  " + username + " " + password);
+    	
+    	if(authenticationService.isValidCredentials(username, password)) {
+    		
+    		System.out.println("++++++++++++++");
+    		return "redirect:/board/list";   
+        } else {
+            
+            model.addAttribute("error", "자격 증명에 실패하였습니다.");
+            return "login";
+        }
+    }
+}
